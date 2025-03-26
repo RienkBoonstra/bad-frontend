c1f58905a553a208fe68047df7456774766e6410
Rienk Boonstra <boonstra.application.development@gmail.com>
2025-03-26 01:06:46 +0100

    Negligeable modification.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,13 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1>Boonstra Application Development</h1>
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
+            Java/Kotlin Development
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            React/Nextjs Development
           </li>
         </ol>
       </main>
